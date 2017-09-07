2aef20cbc65fa0508ecf4cde5dba56efe10b94a7
Alex Leigh <alex@revinate.com>
2017-09-07 12:11:51 -0700

    Fix ziplist applicative tests.

--- test/ziplist-applicative.js
+++ test/ziplist-applicative.js
@@ -3,19 +3,20 @@ var ZiplistApplicative = require("../applicatives/ziplist-applicative");
 
 describe("Ziplist applicative", function() {
  it("should implement apply", function() {
-   ziplistApplicative = ZiplistApplicative.of(function(x) {return 2*x;},function(x) {return 2+x;}).apply(ZiplistApplicative.of(1, 2));
+   var orig = new ZiplistApplicative([function(x) {return 2*x;},function(x) {return 2+x;}]);
+   var ziplistApplicative = orig.apply(new ZiplistApplicative([1, 2]));
    expect(ziplistApplicative.__value).to.be.an('array').to.deep.equal([2,4]);
  });
 
  it("should support currying with apply", function() {
    var add = function(x) {return function(y) {return x+y;};};
-   ziplistApplicative = ZiplistApplicative.of(add, add).apply(ZiplistApplicative.of(1, 2)).apply(ZiplistApplicative.of(3, 4));
+   var ziplistApplicative = ZiplistApplicative.of(add).apply(new ZiplistApplicative([1, 2])).apply(new ZiplistApplicative([3, 4]));
    expect(ziplistApplicative.__value).to.be.an('array').to.deep.equal([4,6]);
  });
 
  it("should support currying with map and apply", function() {
    var add = function(x) {return function(y) {return x+y;};};
-   ziplistApplicative = ZiplistApplicative.of(1,2).map(add).apply(ZiplistApplicative.of(3, 4));
+   var ziplistApplicative = (new ZiplistApplicative([1,2])).map(add).apply(new ZiplistApplicative([3, 4]));
    expect(ziplistApplicative.__value).to.be.an('array').to.deep.equal([4,6]);
  });
 });
